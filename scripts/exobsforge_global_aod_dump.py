bf96feaa9fcbcb0c7e46f5a1d1c1e38ee6c668b4
CoryMartin-NOAA <cory.r.martin@noaa.gov>
2025-03-24 14:01:28 +0000

    commit copied stuff from g-w

--- scripts/exobsforge_global_aod_dump.py
+++ scripts/exobsforge_global_aod_dump.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+# exobsforge_global_aod_dump.py
+# This script will collect and preprocess
+# aerosol optical depth observations for
+# global aerosol assimilation 
+import os
+
+from wxflow import Logger, cast_strdict_as_dtypedict
+from pyobsforge.task.aero_prepobs import AerosolObsPrep
+
+# Initialize root logger
+logger = Logger(level='DEBUG', colored_log=True)
+
 
 if __name__ == '__main__':
-    print('hello')
+
+    # Take configuration from environment and cast it as python dictionary
+    config = cast_strdict_as_dtypedict(os.environ)
+
+    AeroObs = AerosolObsPrep(config)
+    AeroObs.initialize()
+    AeroObs.runConverter()
+    AeroObs.finalize()
