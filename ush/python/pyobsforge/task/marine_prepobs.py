47741ac97aaf6313972bf52e2bfcd733183972f4
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2025-03-28 16:05:20 -0400

    call nc2ioda app

--- ush/python/pyobsforge/task/marine_prepobs.py
+++ ush/python/pyobsforge/task/marine_prepobs.py
@@ -2,10 +2,11 @@
 
 from logging import getLogger
 from typing import Dict, Any
-
+import subprocess
 from wxflow import (AttrDict, Task, add_to_datetime, to_timedelta,
-                    logit)
+                    logit, FileHandler, Jinja)
 from pyobsforge.obsdb.ghrsst_db import GhrSstDatabase
+from os.path import basename, join
 
 logger = getLogger(__name__.split('.')[-1])
 
@@ -31,6 +32,7 @@ class MarineObsPrep(Task):
 
         # task_config is everything that this task should need
         self.task_config = AttrDict(**self.task_config, **local_dict)
+        #print(self.task_config)
 
         # Initialize the GHRSST database
         self.ghrsst_db = GhrSstDatabase(db_name="sst_obs.db",
@@ -49,25 +51,71 @@ class MarineObsPrep(Task):
         """
         """
 
-        # Loop through obs spaces
-        for provider in self.task_config.providers.ghrsst:
+        for provider, obs_spaces in self.task_config.providers.items():
             logger.info(f"========= provider: {provider}")
-            # extract the instrument and platform from the obs_space
-            obs_type, instrument, platform, proc_level = provider.split("_")
-            platform = platform.upper()
-            instrument = instrument.upper()
-            logger.info(f"Processing {platform.upper()} {instrument.upper()}")
+            for obs_space in obs_spaces:
+                logger.info(f"========= obs_space: {obs_space}")
+                # extract the instrument and platform from the obs_space
+                obs_type, instrument, platform, proc_level = obs_space.split("_")
+                platform = platform.upper()
+                instrument = instrument.upper()
+                logger.info(f"Processing {platform.upper()} {instrument.upper()}")
 
-            # Query the database for valid files
-            valid_files = self.ghrsst_db.get_valid_files(
-                window_begin=self.task_config.window_begin,
-                window_end=self.task_config.window_end,
-                instrument=instrument,
-                satellite=platform,
-                obs_type="SSTsubskin"
-            )
+                # Query the database for valid files
+                valid_files = self.ghrsst_db.get_valid_files(
+                    window_begin=self.task_config.window_begin,
+                    window_end=self.task_config.window_end,
+                    instrument=instrument,
+                    satellite=platform,
+                    obs_type="SSTsubskin"
+                )
+                logger.info(f"number of valid files: {len(valid_files)}")
 
-            logger.info(f"number of valid files: {len(valid_files)}")
+                # Process the observations if the obs space is not empty
+                if len(valid_files) > 0:
+                    # Copy the valid files to the RUNDIR
+                    src_dst_obs_list = []  # list of [src_file, dst_file]
+                    input_files = []       # list of dst_files used as input to the ioda converter
+                    for src_file in valid_files:
+                        dst_file = f"{obs_space}/{basename(src_file)}"
+                        input_files.append(dst_file)
+                        logger.info(f"copying {src_file} to {dst_file}")
+                        src_dst_obs_list.append([src_file, dst_file])
+                    FileHandler({'mkdir': [obs_space]}).sync()
+                    FileHandler({'copy': src_dst_obs_list}).sync()
+
+                    # Configure the ioda converter
+                    output_file = f"{self.task_config['RUN']}.t{self.task_config['cyc']:02d}z.{obs_space}.tm00.nc"
+                    context = {'provider': provider.upper(),
+                               'window_begin': self.task_config.window_begin,
+                               'window_end': self.task_config.window_end,
+                               'bounds_min': -2,
+                               'bounds_max': 45,
+                               'binning_stride': 15,
+                               'binning_min_number_of_obs': 10,
+                               'input_files': input_files,
+                               'output_file': output_file}
+                    jinja_template = join(self.task_config['HOMEobsforge'], "parm", "nc2ioda", "nc2ioda.yaml.j2")
+                    print(join(self.task_config['HOMEobsforge'], "parm", "nc2ioda", "nc2ioda.yaml.j2"))
+                    yaml_config = Jinja(jinja_template, context).render
+                    nc2ioda_yaml = f"{obs_space}/{obs_space}_nc2ioda.yaml"
+                    with open(nc2ioda_yaml, "w") as fho:
+                        fho.write(yaml_config)
+
+                    # Run the ioda converter
+                    nc2ioda_exe = join(self.task_config['HOMEobsforge'], 'build', 'bin', 'obsforge_obsprovider2ioda.x')
+                    result = subprocess.run([nc2ioda_exe, basename(nc2ioda_yaml)],
+                                            cwd=obs_space,
+                                            capture_output=True,
+                                            text=True)
+
+                    # Print the standard output
+                    print("Standard Output:")
+                    print(result.stdout)
+
+                    # Optionally, print the standard error
+                    print("Standard Error:")
+                    print(result.stderr)
 
     @logit(logger)
     def finalize(self) -> None:
