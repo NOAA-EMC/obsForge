b2eff89fdfd132d67cc7d1a8807854aa9f7b7325
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2025-04-02 16:47:32 -0400

    ...

--- ush/python/pyobsforge/task/run_nc2ioda.py
+++ ush/python/pyobsforge/task/run_nc2ioda.py
@@ -1,6 +1,6 @@
 from logging import getLogger
 import subprocess
-from wxflow import Jinja, save_as_yaml
+from wxflow import save_as_yaml, parse_j2yaml
 from os.path import join
 
 logger = getLogger(__name__.split('.')[-1])
@@ -18,7 +18,7 @@ def run_nc2ioda(task_config: dict, obs_space: str, context: dict) -> int:
         int: Returns 0 upon successful execution. Logs errors and warnings for failures.
     """
     jinja_template = join(task_config['HOMEobsforge'], "parm", "nc2ioda", "nc2ioda.yaml.j2")
-    yaml_config = Jinja(jinja_template, context).render
+    yaml_config = parse_j2yaml(jinja_template, context)
     nc2ioda_yaml = join(task_config['DATA'], obs_space, f"{obs_space}_nc2ioda.yaml")
     save_as_yaml(yaml_config, nc2ioda_yaml)
 
