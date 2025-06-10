75222020fe9e3f7a62929c2c3a8f73ad199dc7c8
Mindo Choi <mindoc@hercules-login-2.hpc.msstate.edu>
2025-06-10 14:06:31 -0500

    WIP_providers

--- ush/python/pyobsforge/task/providers.py
+++ ush/python/pyobsforge/task/providers.py
@@ -74,6 +74,8 @@ class ProviderConfig:
                 "seaice_mirs"
             ]
             db = NesdisMirsDatabase(db_name=f"{provider_name}.db", dcom_dir=task_config.DCOMROOT, obs_dirs=obs_dirs)
+        elif provider_name == "nesdis_jpssrr":
+            db = RADSDatabase(db_name=f"{provider_name}.db", dcom_dir=task_config.DCOMROOT, obs_dir="wgrdbul/IST")
         elif provider_name == "smap":
             db = SmapDatabase(db_name=f"{provider_name}.db", dcom_dir=task_config.DCOMROOT, obs_dir="wtxtbul/satSSS/SMAP")
         elif provider_name == "smos":
