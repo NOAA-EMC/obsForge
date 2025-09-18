1ac969234ea1138052640cab97bf8088b678625c
wangyap <wangyap1990@gmail.com>
2025-09-18 18:09:57 +0000

    add attributes related to superobbing

--- ush/python/pyobsforge/task/aero_prepobs.py
+++ ush/python/pyobsforge/task/aero_prepobs.py
@@ -71,6 +71,11 @@ class AerosolObsPrep(Task):
                            'thinning_threshold': self.task_config.thinning_threshold,
                            'input_files': input_files,
                            'output_file': output_file}
+                for attr in ['binning_stride', 'binning_min_number_of_obs', 'binning_cressman_radius']:
+                    try:
+                        context[attr] = self.task_config[attr]
+                    except KeyError:
+                        pass
                 result = run_nc2ioda(self.task_config, obs_space, context)
                 logger.info(f"run_nc2ioda result: {result}")
 
