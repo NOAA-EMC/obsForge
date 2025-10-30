669862aed6912a8e7bafed41bbb3685184326fd4
Cory Martin <Cory.R.Martin@noaa.gov>
2025-10-30 16:18:36 +0000

    build with spoc example

--- ush/of_setup.sh
+++ ush/of_setup.sh
@@ -13,6 +13,27 @@ source "${HOMEobsforge}/ush/detect_machine.sh"
 source "${HOMEobsforge}/ush/module-setup.sh"
 module use "${HOMEobsforge}/modulefiles"
 module load "obsforge/${MACHINE_ID}"
-export PYTHONPATH="${HOMEobsforge}/ush/python:$PYTHONPATH"
+
+# Detect the Python major.minor version
+_regex="[0-9]+\.[0-9]+"
+# shellcheck disable=SC2312
+if [[ $(python --version) =~ ${_regex} ]]; then
+    export PYTHON_VERSION="${BASH_REMATCH[0]}"
+else
+    echo "FATAL ERROR: Could not detect the python version"
+    return 1
+fi
+
+###############################################################
+# setup python path for ioda utilities
+# TODO: a better solution should be created for setting paths to package python scripts
+# shellcheck disable=SC2311
+pyiodaPATH="${HOMEobsforge}/build/lib/python${PYTHON_VERSION}/"
+# Add wxflow to PYTHONPATH
+wxflowPATH="${HOMEobsforge}/ush/python"
+PYTHONPATH="${PYTHONPATH:+${PYTHONPATH}:}${HOMEobsforge}/ush:${wxflowPATH}:${pyiodaPATH}"
+export PYTHONPATH
+
+export PYTHONPATH="${PYTHONPATH}:${HOMEobsforge}/build/lib/python${PYTHON_VERSION}/site-packages"
 
 set +ue
