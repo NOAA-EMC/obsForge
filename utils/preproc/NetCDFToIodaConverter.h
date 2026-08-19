10c456628b0bc63f154cfb7684c8504e46a71db3
Guillaume Vernieres <guillaume.vernieres@noaa.gov>
2026-08-19 09:26:09 -0400

    fixed norms

--- utils/preproc/NetCDFToIodaConverter.h
+++ utils/preproc/NetCDFToIodaConverter.h
@@ -313,7 +313,6 @@ namespace obsforge {
                 oceanMask->getOceanMask(iodaVars->longitude_[i], iodaVars->latitude_[i]);
             }
           }
-
         }
         tmpIntMeta.writeWithEigenRegular(iodaVars->intMetadata_.col(count));
         count++;
@@ -356,7 +355,6 @@ namespace obsforge {
           iodaPreQc[i].writeWithEigenRegular(iodaVars->preQcAdditional_.col(i - 1));
         }
       }
-
     }
 
 
